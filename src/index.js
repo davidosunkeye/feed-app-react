7aaf9e5979e40677087a7efdafabb93cf3ddae16
davidosunkeye <123675898+davidosunkeye@users.noreply.github.com>
2023-04-11 11:25:38 -0400

    updated Router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./appRoutes/AppRoutes";
+import AppContextProvider from "./context/applicationContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <AppContextProvider>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </AppContextProvider>
+);

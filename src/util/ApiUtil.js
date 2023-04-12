01da00bcc9dec2f6450cdb40dec73b269abe8eda
davidosunkeye <123675898+davidosunkeye@users.noreply.github.com>
2023-04-12 14:17:09 -0400

    Updated files

--- src/util/ApiUtil.js
+++ src/util/ApiUtil.js
@@ -45,3 +45,90 @@ export const registerApi = async (
     return response;
   }
 };
+
+export const verifyEmailApi = async (token) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/verify/email`;
+    const apiResponse = await axios.get(url, {
+      headers: { Authorization: frameToken(token) },
+    });
+    if (apiResponse.status === 200) {
+      response = frameResponse(1, apiResponse.data);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
+
+export const loginApi = async (username, password) => {
+  let response = frameResponse();
+  try {
+    const url = `${API_BASE_URL}/user/login`;
+    const apiResponse = await axios.post(url, { username, password });
+    if (apiResponse.status === 200) {
+      const payLoad = {
+        userData: apiResponse.data,
+        token: apiResponse.headers.authorization,
+      };
+      response = frameResponse(1, payLoad);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
+
+export const forgotPasswordApi = async (email) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/reset/${email}`;
+    const apiResponse = await axios.get(url);
+    if (apiResponse.status === 200) {
+      response = frameResponse(1);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
+
+export const resetPasswordApi = async (token, password) => {
+  let response = frameResponse();
+
+  try {
+    const url = `${API_BASE_URL}/user/reset`;
+    const apiResponse = await axios.post(
+      url,
+      {
+        password,
+      },
+      { headers: { Authorization: frameToken(token) } }
+    );
+    if (apiResponse.status === 200) {
+      response = frameResponse(1);
+    }
+  } catch (err) {
+    if (err.response) {
+      response = frameResponse(0, err.response.data.message);
+    }
+    console.log(err);
+  } finally {
+    return response;
+  }
+};
